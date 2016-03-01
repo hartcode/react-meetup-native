4c7b5858cbb1d2c5787799db395695f0aa04103f
alex <alex@hartcode.com>
2016-02-29 22:16:03 -0600

    Remove etsy-api and search-native

--- etsy-api/mockApi/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-//todo: let's make these .json files, introduce more better magic things
-
-//billing
-module.exports.listing = require('./getShop.json');
-module.exports.images = require('./getImages.json');
