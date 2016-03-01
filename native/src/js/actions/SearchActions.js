b95421be71dc17edbfc1ad0b791094741fb82922
alex <alex@hartcode.com>
2016-02-29 22:39:13 -0600

    Add in api key

--- native/src/js/actions/SearchActions.js
+++ native/src/js/actions/SearchActions.js
@@ -3,7 +3,7 @@ import ActionTypes from '../constants/ActionTypes';
 
 var ETSY_URL = 'openapi.etsy.com';
 var ETSY_SHOP_ID = '12125241';
-var ETSY_API_KEY = process.env.ETSY_API_KEY;
+var ETSY_API_KEY = '7xsp1p3sut5xz5yf17jyuxcj';
 var ETSY_LISTINGS_PATH = '/v2/shops/' + ETSY_SHOP_ID + '/listings/active?api_key=' + ETSY_API_KEY;
 var ETSY_IMAGES_PATH = '/v2/listings/';
 
